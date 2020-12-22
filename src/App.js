f6465739519b43bbab76b0cfd3ed089bc50b6132
Caio <caiodev.contato@gmail.com>
2020-12-22 09:08:34 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+
+function App() {
+  return <>
+    {/* Header */}
+
+    {/* Option List */}
+
+    {/* Product Viewer */}
+
+    {/* Cart */}
+
+    {/* Buy Page */}
+  </>;
+}
+
+export default App;
