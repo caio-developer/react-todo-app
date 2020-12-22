3db73ebef078051983077b02c13aac7c526d2b1d
Caio <caiodev.contato@gmail.com>
2020-12-22 09:40:37 -0300

    Notification Timer Added

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -4,10 +4,17 @@ export const GlobalContext = React.createContext();
 
 function Storage({ children }) {
   const [cart, setCart] = React.useState([]);
-  const [notification, setNotification] = React.useState(() => <p>Notificação</p>);
+  const [notification, setNotification] = React.useState(null);
+  const notificationRef = React.useRef();
+
+  React.useEffect(() => {
+    clearTimeout(notificationRef.current);
+    notificationRef.current = setTimeout(() => setNotification(null), 2000);
+  }, [notification]);
 
   return (
     <GlobalContext.Provider value={{ cartCounter: cart.length, notification }}>
+      <button onClick={() => setNotification(<p>Notificação</p>)}></button>
       {children}
     </GlobalContext.Provider>
   );
