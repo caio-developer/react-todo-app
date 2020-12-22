80bfa0e13b54701ac9c0ade3ab71dc32a1a7a929
Caio <caiodev.contato@gmail.com>
2020-12-22 09:36:24 -0300

    Header Update - Notification Viewer Funcionality Added

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -4,9 +4,10 @@ export const GlobalContext = React.createContext();
 
 function Storage({ children }) {
   const [cart, setCart] = React.useState([]);
+  const [notification, setNotification] = React.useState(() => <p>Notificação</p>);
 
   return (
-    <GlobalContext.Provider value={{ cartCounter: cart.length }}>
+    <GlobalContext.Provider value={{ cartCounter: cart.length, notification }}>
       {children}
     </GlobalContext.Provider>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { GlobalContext } from '../GlobalContext';
 
 function Header() {
-  const { cartCounter } = React.useContext(GlobalContext);
+  const { cartCounter, notification } = React.useContext(GlobalContext);
 
-  return <div className='header'>
-    <div className='counter__viewer'>
-      <p>Counter: {cartCounter}</p>
+  return (
+    <div className='header'>
+      <div className='counter__viewer'>
+        <p>Counter: {cartCounter}</p>
+      </div>
+      <div className='notification__viewer'>{notification}</div>
     </div>
-  </div>;
+  );
 }
 
 export default Header;
