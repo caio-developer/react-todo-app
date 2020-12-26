0314a38e52c20770bd7021b64f4733b01f603644
Caio <caiodev.contato@gmail.com>
2020-12-26 16:37:37 -0300

    Todo Item Remove Funcionality Added

--- src/ToDoContext.js
+++ src/ToDoContext.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {v4 as uuid} from 'uuid';
 
 export const ToDoContext = React.createContext();
 
@@ -6,15 +7,19 @@ export default function ToDoContextProvider({ children }) {
   const [todo, setTodo] = useState([]);
 
   function addNewTodo(text) {
-    setTodo(prevState => [...prevState, text]);
+    setTodo(prevState => [...prevState, {text: text, id: uuid()}]);
   }
 
   function clearTodoList() {
     setTodo([]);
   }
 
+  function removeTodoItem(id) {
+    setTodo(todo.filter(item => item.id !== id));
+  }
+
   return (
-    <ToDoContext.Provider value={{ addNewTodo, todo, clearTodoList }}>
+    <ToDoContext.Provider value={{ addNewTodo, todo, clearTodoList, removeTodoItem }}>
       {children}
     </ToDoContext.Provider>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { ToDoContext } from '../ToDoContext';
 
-function TodoItem({ item, index }) {
-  const { editTodoList } = React.useContext(ToDoContext);
-
-  function edit(index) {
-    let newValue = prompt('Qual o novo valor?');
-    editTodoList(newValue, index)
-  }
+function TodoItem({ item, index, id }) {
+  const { editTodoList, removeTodoItem } = React.useContext(ToDoContext);
 
   return (
     <li key={item + index}>
-      <span>{item}</span>
+      <span>{item.text}</span>
+      <button onClick={() => removeTodoItem(item.id)}>Delete</button>
     </li>
   );
 }
