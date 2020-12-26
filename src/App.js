f341ef007f11ec54ec7b2af0c0d67f53f882f44c
Caio <caiodev.contato@gmail.com>
2020-12-25 22:10:40 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import Storage from './GlobalContext';
 import './style.css';
-import Header from './components/Header';
-import Options from './components/Options';
 
 function App() {
-  const [itemList, setItemList] = React.useState([]);
-
-  React.useEffect(() => {
-    (async () => {
-      const response = await fetch('https://ranekapi.origamid.dev/json/api/produto')
-        .then(response => response.json());
-
-      setItemList(response);
-    })();
-  }, []);
-
   return (
-    <Storage>
-      {/* Header */}
-      <Header />
-      {/* Option List */}
-      <Options items={itemList} />
-      {/* Product Viewer */}
-
-      {/* Cart */}
-
-      {/* Buy Page */}
-    </Storage>
+    <>
+      
+    </>
   );
 }
 
