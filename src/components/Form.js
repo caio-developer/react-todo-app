5713707dc886f396c8ccf13257395811be7a6692
Caio <caiodev.contato@gmail.com>
2020-12-26 16:44:49 -0300

    Form Focus and Reset Value Funcionalities Added

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,7 @@ import { ToDoContext } from '../ToDoContext';
 function Form() {
   const { addNewTodo, clearTodoList } = React.useContext(ToDoContext);
   const [value, setValue] = useState('');
+  const inputRef = React.useRef();
 
   return (
     <div className='form-wrapper'>
@@ -11,8 +12,17 @@ function Form() {
         type='text'
         value={value}
         onChange={e => setValue(e.target.value)}
+        ref={inputRef}
       />
-      <button onClick={() => addNewTodo(value)}>Add</button>
+      <button
+        onClick={() => {
+          addNewTodo(value);
+          setValue('');
+          inputRef.current.focus();
+        }}
+      >
+        Add
+      </button>
       <button onClick={() => clearTodoList()}>Clear</button>
     </div>
   );
